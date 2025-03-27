f67209c67ed7e774cc67ce6499ab8657903e4587
plirexic <92991314+Plirexic@users.noreply.github.com>
2025-03-27 14:03:23 +0100

    spring init

---
+++ focusflowbackend/src/main/java/de/hsesslingen/focusflowbackend/FocusflowbackendApplication.java
@@ -0,0 +1,13 @@
+package de.hsesslingen.focusflowbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FocusflowbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FocusflowbackendApplication.class, args);
+	}
+
+}
---
+++ focusflowbackend/src/test/java/de/hsesslingen/focusflowbackend/FocusflowbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hsesslingen.focusflowbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FocusflowbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
